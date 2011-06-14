fdbe2398b4756d5c93098cb15c7506141f515ebc
Ken Lim <kenneth.lim@guardian.co.uk>
2011-06-14 14:47:29 +0100

    Now produces diffs for Json files

--- build.sbt
+++ build.sbt
@@ -2,6 +2,11 @@ name := "Content Api Diff Tool"
 
 version := "1.0"
 
-libraryDependencies +=  "net.databinder" %% "dispatch-http" % "0.8.3"
+libraryDependencies ++= Seq(
+    "net.databinder" %% "dispatch-http" % "0.8.3",
+    "net.liftweb" %% "lift-json" % "2.4-M2"
+    )
+
+
 
 scalaVersion := "2.9.0-1"
--- src/main/scala/com/gu/contentapi/difftool/Main.scala
+++ src/main/scala/com/gu/contentapi/difftool/Main.scala
@@ -2,9 +2,11 @@ package com.gu.contentapi.difftool
 
 import io.Source
 
-import OwenDiff.Diff
+import OwenDiff.{ Diff => OwensDiff }
 import dispatch._
+import net.liftweb.json._
 import java.io.{FileWriter, File, StringWriter}
+import net.liftweb.json.JsonAST.JValue
 
 object Main {
   val masterContentApiHost = url("http://localhost:8080/api")
@@ -26,7 +28,27 @@ object Main {
 
   def doTextDiff(master: String, liftRest: String) = {
     def tidy(s: String) = s.replaceAllLiterally("<", "\n<").split('\n')
-    Diff.diff(tidy(master), tidy(liftRest))
+    OwensDiff.diff(tidy(master), tidy(liftRest))
+  }
+
+  def r(json: JValue) = json match {
+    case JNothing => "No difference"
+    case other => pretty(render(other))
+  }
+
+  def doJsonDiff(master: String, liftRest: String) = {
+    val (masterJson, liftJson) = (parse(master), parse(liftRest))
+    val Diff(changed, added, deleted) = masterJson diff liftJson
+
+    """|LINES MODIFIED:
+       |%s
+
+       |LINES ADDED:
+       |%s
+
+       |LINES DELETED:
+       |%s
+    |""".stripMargin.format(r(changed), r(added), r(deleted))
   }
 
   def processline( idxAndLine: (String, Int) ) {
@@ -39,7 +61,12 @@ object Main {
     // 2. Lift-rest Content Api
     val liftRestResponse = getResponse(liftRestContentApiHost / pathAndParams)
 
-    val diffResult = doTextDiff(masterResponse, liftRestResponse)
+
+    val diffResult =
+      """|Differences in results when calling
+         |%s
+         
+      |""".stripMargin.format(pathAndParams) + doJsonDiff(masterResponse, liftRestResponse)
 
     writeToFile(new File("result/%d.diff" format idx), diffResult.mkString)
   }
@@ -48,7 +75,7 @@ object Main {
     new File("result").mkdirs()
 
     // Load up the provided log file
-    val logFile = Source.fromFile("support/inputFile").getLines().take(10)
+    val logFile = Source.fromFile("support/inputJson").getLines().take(10)
 
     logFile.zipWithIndex.toList.par.foreach(processline)
     
