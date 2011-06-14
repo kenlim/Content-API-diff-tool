7e895d6a3fe723c93117f579e657d818576cff58
Ken Lim <kenneth.lim@guardian.co.uk>
2011-06-14 12:26:35 +0100

    Code refactored to output nice diff files.

--- build.sbt
+++ build.sbt
@@ -2,6 +2,6 @@ name := "Content Api Diff Tool"
 
 version := "1.0"
 
-scalaVersion := "2.9.0"
-
 libraryDependencies +=  "net.databinder" %% "dispatch-http" % "0.8.3"
+
+scalaVersion := "2.9.0-1"
--- src/main/scala/com/gu/contentapi/difftool/Main.scala
+++ src/main/scala/com/gu/contentapi/difftool/Main.scala
@@ -1,41 +1,57 @@
 package com.gu.contentapi.difftool
 
 import io.Source
-import java.net.URL
-import dispatch.{url, Http}
-import javax.management.remote.rmi._RMIConnection_Stub
+
 import OwenDiff.Diff
+import dispatch._
+import java.io.{FileWriter, File, StringWriter}
 
 object Main {
-  val masterContentApiHost = "http://localhost:8080/api"
-  val liftRestContentApiHost = "http://localhost:8700/content-api/api"
+  val masterContentApiHost = url("http://localhost:8080/api")
+  val liftRestContentApiHost = url("http://localhost:8700/content-api/api")
 
-  val h = new Http
-
-  def processline(line: String) {
-    var pathAndParams = line.replaceAllLiterally("/content-api/api", "")
-
-    if (! pathAndParams.matches(".*(json|xml).*")) {
-      pathAndParams += "&format=json"
-    }
-    
-    // Translate the URLs into calls to make to the:
-    // 1. Master Content Api
-    val masterResponse = h(url(masterContentApiHost + pathAndParams) as_str).replaceAllLiterally("<", "\n<").split('\n')
-    // 2. Lift-rest Content Api
-    val liftRestResponse = h(url(liftRestContentApiHost + pathAndParams) as_str).replaceAllLiterally("<", "\n<").split('\n')
+  val h = new Http with thread.Safety
 
 
-    val diffList = Diff.diff(masterResponse, liftRestResponse)
-
-    println(diffList.mkString)
+  def writeToFile(file: File, s: String) {
+    val writer = new FileWriter(file)
+    writer.write(s)
+    writer.close()
   }
 
-  def main(args: Array[String]) = {
+  def getResponse(req: Request) = {
+    val myReq = req <:< Map("Accept" -> "application/json")
+    h(myReq as_str)
+  }
+
+  def doTextDiff(master: String, liftRest: String) = {
+    def tidy(s: String) = s.replaceAllLiterally("<", "\n<").split('\n')
+    Diff.diff(tidy(master), tidy(liftRest))
+  }
+
+  def processline( idxAndLine: (String, Int) ) {
+    val (line, idx) = idxAndLine
+    val pathAndParams = line.replaceAllLiterally("/content-api/api", "")
+
+    // Translate the URLs into calls to make to the:
+    // 1. Master Content Api
+    val masterResponse = getResponse(masterContentApiHost / pathAndParams)
+    // 2. Lift-rest Content Api
+    val liftRestResponse = getResponse(liftRestContentApiHost / pathAndParams)
+
+    val diffResult = doTextDiff(masterResponse, liftRestResponse)
+
+    writeToFile(new File("result/%d.diff" format idx), diffResult.mkString)
+  }
+
+  def main(args: Array[String]) {
+    new File("result").mkdirs()
+
     // Load up the provided log file
     val logFile = Source.fromFile("support/inputFile").getLines().take(10)
 
-    logFile.foreach(processline _)
-
+    logFile.zipWithIndex.toList.par.foreach(processline)
+    
+    h.shutdown()
   }
 }
